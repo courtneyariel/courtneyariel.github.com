195470bf66dd5d675558780ef8a578ff23d04c9c
courtneyariel <courtneyarielbogus@gmail.com>
2015-08-18 15:07:52 -0700

    css and js edits

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@
             $(document).scroll(function() { 
                 scroll_pos = $(this).scrollTop();
                 if(scroll_pos > 585) {
-                    $(".top_bar").addClass("black_bkgd");
+                    $(".top_bar").addClass("whiteopacity");
                 } else {
-                    $(".top_bar").removeClass("black_bkgd");
+                    $(".top_bar").removeClass("whiteopacity");
                 }
             });
         });
@@ -63,32 +63,21 @@ $('.scrollup').click(function () {
     });
 
 
-jQuery(function( $ ){
-            /**
-             * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
-             * @see http://demos.flesler.com/jquery/scrollTo/
-             * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.localScroll.
-             */
-            
-            // The default axis is 'y', but in this demo, I want to scroll both
-            // You can modify any default like this
+$(function( $ ){
+
             $.localScroll.defaults.axis = 'y';
             
-            /**
-             * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
-             * also affect the >> and << links. I want every link in the page to scroll.
-             */
             $.localScroll({
-                target: 'html', // could be a selector or a jQuery object too.
+                target: 'html', 
                 queue:true,
                 duration:1000,
                 hash:true,
                 offset: -85,                
                 onBefore:function( e, anchor, $target ){
-                    // The 'this' is the settings object, can be modified
+                  
                 },
                 onAfter:function( anchor, settings ){
-                    // The 'this' contains the scrolled element (#content)
+                    
                 }
             });
         });
